4f230dbe9d3acd7c185a9a3a34706103ec9210cc
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2021-04-22 17:22:45 +0200

    Initialized Project

---
+++ src/main/java/com/kodikas/appvaccinibackend/AppVacciniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodikas.appvaccinibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVacciniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVacciniBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodikas/appvaccinibackend/AppVacciniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodikas.appvaccinibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVacciniBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
