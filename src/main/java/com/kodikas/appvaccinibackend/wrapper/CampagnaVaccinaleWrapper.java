8fd23c42288fc16b971b331f24711c31c3130f58
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2021-04-28 15:27:32 +0200

    removed boilerplate from CampagnaVaccinaleWrapper

--- src/main/java/com/kodikas/appvaccinibackend/wrapper/CampagnaVaccinaleWrapper.java
+++ src/main/java/com/kodikas/appvaccinibackend/wrapper/CampagnaVaccinaleWrapper.java
@@ -1,21 +1,15 @@
 package com.kodikas.appvaccinibackend.wrapper;
 
 import com.kodikas.appvaccinibackend.model.CampagnaVaccinale;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class CampagnaVaccinaleWrapper {
     List<CampagnaVaccinale> campagneVaccinali;
-
-    public CampagnaVaccinaleWrapper(List<CampagnaVaccinale> campagneVaccinali) {
-        this.campagneVaccinali = campagneVaccinali;
-    }
-
-    public List<CampagnaVaccinale> getCampagneVaccinali() {
-        return campagneVaccinali;
-    }
-
-    public void setCampagneVaccinali(List<CampagnaVaccinale> campagneVaccinali) {
-        this.campagneVaccinali = campagneVaccinali;
-    }
 }
--- src/test/java/com/kodikas/appvaccinibackend/service/CampagnaVaccinaleServiceUnitTest.java
+++ src/test/java/com/kodikas/appvaccinibackend/service/CampagnaVaccinaleServiceUnitTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
