a0f5b69222980b4a5189c5dfea47bc26c62e2176
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2021-05-11 13:16:31 +0200

    added NoArgsContructor to CampagnaVaccinaleWrapper

--- src/main/java/com/kodikas/appvaccinibackend/wrapper/CampagnaVaccinaleWrapper.java
+++ src/main/java/com/kodikas/appvaccinibackend/wrapper/CampagnaVaccinaleWrapper.java
@@ -3,10 +3,12 @@ package com.kodikas.appvaccinibackend.wrapper;
 import com.kodikas.appvaccinibackend.model.CampagnaVaccinale;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CampagnaVaccinaleWrapper {
     List<CampagnaVaccinale> campagneVaccinali;
