0a61467f2d378e8ebe46299c5c849b36b8a305c1
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2021-05-15 17:38:18 +0200

    Added Acceptance Test for CitizenController

---
+++ src/test/java/com/kodikas/appvaccinibackend/controller/CitizenControllerAcceptanceTest.java
@@ -0,0 +1,29 @@
+package com.kodikas.appvaccinibackend.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CitizenControllerAcceptanceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getCitizens() {
+    }
+
+    @Test
+    void getCitizen() {
+    }
+
+    @Test
+    void modifyRegistered() {
+    }
+}
